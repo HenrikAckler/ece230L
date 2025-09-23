eebef18cc960925e816af8b58b670d5169b16342
HenrikAckler <169107536+HenrikAckler@users.noreply.github.com>
2025-09-23 17:05:42 -0600

    lab5 prep mostly done

--- lab5/class/circuit_b.v
+++ lab5/class/circuit_b.v
@@ -1,8 +1,11 @@
 module circuit_b(
     // Declare inputs
+    input A, B, C, D,
+    output Y
     // Declare Y output
 );
 
-    // Enter logic equation here
+// Enter logic equation here
+assign Y = (~D & (~C | B)) | (A & B);
 
 endmodule
--- lab5/class/top.v
+++ lab5/class/top.v
@@ -1,1 +1,34 @@
-// Implement top level module
+module top(
+    input [6:0] sw,
+    output [1:0] led
+);
+    wire AB;
+    
+    circuit_a inst_circuit_a (
+        .A(sw[0]), 
+        .B(sw[1]), 
+        .C(sw[2]),
+        .D(sw[3]), 
+        .Y(AB)
+    );
+    
+    assign led[0] = AB;
+    
+    circuit_b inst_circuit_b (
+        .A(AB), 
+        .B(sw[4]), 
+        .C(sw[5]),
+        .D(sw[6]), 
+        .Y(led[1])
+    );
+    
+    //assign aToB = circuit_a(sw[0], sw[1], sw[2], sw[3]);
+//    assign led[0] = aToB;
+    
+//    wire bOut;
+    
+//    circuit_b inst_circuit_b(aToB, sw[4], sw[5], sw[6], led[1]);
+    //assign bOut = circuit_b(aToB, sw[4], sw[5], sw[6]);
+//    assign led[1] = bOut;
+
+endmodule
