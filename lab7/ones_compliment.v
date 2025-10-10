751a43d65b314d66371157f514fa59bb4faed208
HenrikAckler <169107536+HenrikAckler@users.noreply.github.com>
2025-10-10 09:40:06 -0600

    lab7 finished

--- lab7/ones_compliment.v
+++ lab7/ones_compliment.v
@@ -11,10 +11,10 @@ module ones_compliment(
     assign c[0] = 1'b0;	// initial carry in  = 0
     
     //ripple-carry chain
-    full_adder fa0 (.A(A[0]), .B(~B[0]), .Cin(c[0]), .Y(sum[0]), .Cout(c[1]));
-    full_adder fa1 (.A(A[1]), .B(~B[1]), .Cin(c[1]), .Y(sum[1]), .Cout(c[2]));
-    full_adder fa2 (.A(A[2]), .B(~B[2]), .Cin(c[2]), .Y(sum[2]), .Cout(c[3]));
-    full_adder fa3 (.A(A[3]), .B(~B[3]), .Cin(c[3]), .Y(sum[3]), .Cout(c[4]));
+    full_adder fa0 (.A(A[0]), .B(B[0]), .Cin(c[0]), .Y(sum[0]), .Cout(c[1]));
+    full_adder fa1 (.A(A[1]), .B(B[1]), .Cin(c[1]), .Y(sum[1]), .Cout(c[2]));
+    full_adder fa2 (.A(A[2]), .B(B[2]), .Cin(c[2]), .Y(sum[2]), .Cout(c[3]));
+    full_adder fa3 (.A(A[3]), .B(B[3]), .Cin(c[3]), .Y(sum[3]), .Cout(c[4]));
     
     //end-around carry
     full_adder fa0c(.A(sum[0]), .B(1'b0), .Cin(c[4]), .Y(S[0]), .Cout(c2[0]));
